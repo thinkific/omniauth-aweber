c29f9a8d12e9456f25656cae714fb76347618825
Dustin Ward <dustin.w@boxcar21.com>
2013-07-12 17:32:10 -0500

    First commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in omniauth-aweber.gemspec
+gemspec
---
+++ lib/omniauth/strategies/aweber.rb
@@ -0,0 +1,7 @@
+require "omniauth/aweber/version"
+
+module Omniauth
+  module Aweber
+    # Your code goes here...
+  end
+end
